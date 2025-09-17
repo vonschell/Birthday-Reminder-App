efa50e7a273d745b64bd19be36e4210ab5058d5d
vonschell <vonschell02@gmail.com>
2025-09-17 14:02:19 +0200

    Add birthday lookup with if/elif/else

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 user_input = input("Which birthday are you looking for? ")
+
+if user_input == "Ulrich":
+    print("Ulrich's birthday is on June 28th.")
+elif user_input == "Jade":
+    print("Jade's birthday is on November 23rd.")
+elif user_input == "Yumi":
+    print("Yumi's birthday is on March 21st.")
+else:
+    print("Sorry, I don't have that birthday information.")
